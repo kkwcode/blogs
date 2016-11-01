f33827c70c0c21c51e2a308ce5da5ec9ac831d90
DullSpy <wujingang1118@outlook.com>
2016-11-01 16:23:31 +0800

    Create VisualTreeHelper.cs

---
+++ DotNet/WPF/VisualTreeHelper.cs
@@ -0,0 +1,50 @@
+        // walk up the visual tree to find object of type T, starting from initial object
+        public static T FindUpVisualTree<T>(DependencyObject initial) where T : DependencyObject
+        {
+            DependencyObject current = initial;
+
+            while (current != null && current.GetType() != typeof(T))
+            {
+                current = VisualTreeHelper.GetParent(current);
+            }
+            return current as T;
+        }
+
+        public static T FindDownVisualTree<T>(Visual parent) where T : Visual
+        {
+            T child = default(T);
+            int numVisuals = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < numVisuals; i++)
+            {
+                Visual v = (Visual)VisualTreeHelper.GetChild(parent, i);
+                child = v as T;
+                if (child == null)
+                {
+                    child = FindDownVisualTree<T>(v);
+                }
+                if (child != null)
+                {
+                    break;
+                }
+            }
+            return child;
+        }
+        
+        private T FindVisualChildByName<T>(DependencyObject parent, string name) where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+
+                string controlName = child.GetValue(Control.NameProperty) as string;
+                if (controlName == name)
+                {
+                    return child as T;
+                }
+
+                T result = FindVisualChildByName<T>(child, name);
+                if (result != null)
+                    return result;
+            }
+            return null;
+        }
